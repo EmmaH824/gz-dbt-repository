70d14ea343be6c960f13ab474d1dc871313847aa
Emma Holness <emma.holness@gmail.com>
2024-09-10 17:18:17 +0000

    All calculations performed successfully.

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -3,6 +3,11 @@ SELECT
     ,COUNT(orders_id) AS nb_transactions
     ,ROUND(SUM(revenue),2) AS total_revenue
     ,ROUND(AVG(revenue),2) AS av_basket
+    ,ROUND(SUM(operational_margin),2) AS operational_margin
+    ,ROUND(SUM(purchase_cost),2) AS total_purchase_cost
+    ,ROUND(SUM(shipping_fee),2) AS total_shipping_fees
+    ,ROUND(SUM(logcost),2) AS total_logcost
+    ,ROUND(SUM(quantity),2) AS total_products_sold
 
 FROM {{ref("int_orders_operational")}}
 JOIN {{ref("stg_raw__ship")}}
