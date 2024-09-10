a9457fdda7edd7b5d9d32f1b2375c81ff9a1dd1d
Emma Holness <emma.holness@gmail.com>
2024-09-10 17:02:41 +0000

    Updated operational margin calculation to make finance days possible.

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,6 +1,10 @@
 SELECT 
     stg_raw__ship.orders_id
     ,MAX(date_date) AS date_date
+    ,SUM(revenue) AS revenue
+    ,SUM(quantity) AS quantity
+    ,SUM(purchase_cost) AS purchase_cost
+    ,SUM(margin) AS margin
     ,SUM(margin + shipping_fee - logcost - ship_cost) AS operational_margin
 FROM
 {{ref("int_orders_margin")}}
--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -1,5 +1,16 @@
 SELECT 
     date_date
-    ,COUNT(orders_id) AS nb_orders
+    ,COUNT(orders_id) AS nb_transactions
+    ,ROUND(SUM(revenue),2) AS revenue
+    ,ROUND(AVG(revenue),2) AS average_basket
+    ,SUM(revenue - (quantity*purchase_price)) AS margin
+    ,SUM(shipping_fee - (logcost + ship_cost)) AS log_ship
+    ,MAX(logcost)
+    ,MAX(ship_cost)
+    -- ,ROUND(SUM((revenue - (quantity*purchase_price)) + shipping_fee - logcost - ship_cost),2) AS operational_margin
 FROM {{ref("stg_raw__sales")}}
+JOIN {{ref("stg_raw__ship")}}
+USING (orders_id)
+JOIN {{ref("stg_raw__product")}}
+USING (products_id)
 GROUP BY date_date
