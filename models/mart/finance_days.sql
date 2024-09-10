b421a7d2d78f69d4b65a6058f40cb677bbe40249
Emma Holness <emma.holness@gmail.com>
2024-09-10 17:11:25 +0000

    Finance daily build - group by, nb transactions, total revenue and average basket in place and working.

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -1,16 +1,10 @@
 SELECT 
     date_date
     ,COUNT(orders_id) AS nb_transactions
-    ,ROUND(SUM(revenue),2) AS revenue
-    ,ROUND(AVG(revenue),2) AS average_basket
-    ,SUM(revenue - (quantity*purchase_price)) AS margin
-    ,SUM(shipping_fee - (logcost + ship_cost)) AS log_ship
-    ,MAX(logcost)
-    ,MAX(ship_cost)
-    -- ,ROUND(SUM((revenue - (quantity*purchase_price)) + shipping_fee - logcost - ship_cost),2) AS operational_margin
-FROM {{ref("stg_raw__sales")}}
+    ,ROUND(SUM(revenue),2) AS total_revenue
+    ,ROUND(AVG(revenue),2) AS av_basket
+
+FROM {{ref("int_orders_operational")}}
 JOIN {{ref("stg_raw__ship")}}
 USING (orders_id)
-JOIN {{ref("stg_raw__product")}}
-USING (products_id)
 GROUP BY date_date
