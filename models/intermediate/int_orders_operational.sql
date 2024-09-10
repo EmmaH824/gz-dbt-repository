6eafd74bc2bbb22be3ed71cbaed74bf34de5c036
Emma Holness <emma.holness@gmail.com>
2024-09-10 15:35:22 +0000

    Added orders operational model.

---
+++ models/intermediate/int_orders_operational.sql
@@ -0,0 +1,10 @@
+SELECT 
+    stg_raw__ship.orders_id
+    ,MAX(date_date) AS date_date
+    ,SUM(margin + shipping_fee - logcost - ship_cost) AS operational_margin
+FROM
+{{ref("int_sales_margin")}}
+JOIN
+{{ref("stg_raw__ship")}}
+USING (orders_id)
+GROUP BY stg_raw__ship.orders_id
